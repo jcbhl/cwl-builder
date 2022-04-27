eacedffef687c7768bd98cbf2d45e8a8c465f711
Jacob <jlh2hb@virginia.edu>
2022-04-27 19:51:04 -0400

    cwl: refactor file listing, about to add double click to cd
    Signed-off-by: Jacob <jlh2hb@virginia.edu>

--- src/renderer.ts
+++ src/renderer.ts
@@ -10,18 +10,18 @@ import "cwl-svg/src/plugins/port-drag/theme.dark.scss";
 import "cwl-svg/src/plugins/selection/theme.dark.scss";
 import { dialog, ipcRenderer, OpenDialogSyncOptions } from 'electron';
 
-function render_workflow(path: string){
+function render_workflow(path: string) {
   const file_contents = fs.readFileSync(path, 'utf8');
-  
-  const sample = function() {
-    if(path.endsWith('json')){
+
+  const sample = function () {
+    if (path.endsWith('json')) {
       return JSON.parse(file_contents);
-    } else if (path.endsWith('cwl')){
+    } else if (path.endsWith('cwl')) {
       return yaml.parse(file_contents);
     }
   }();
 
-  if(!sample){
+  if (!sample) {
     console.log(`error in parsing file at path ${path}`);
     return;
   }
@@ -97,20 +97,31 @@ open_button.addEventListener('click', async () => {
   const path = (await ipcRenderer.invoke("showDialog"))[0];
   console.log(`async call returned ${path}`);
   const file_list = document.getElementById('file-list')!;
-  
+
   file_list.replaceChildren();
 
-  const files = fs.readdirSync(path);
+  const files = fs.readdirSync(path, { withFileTypes: true });
 
-  for(const file of files){
+  for (const file of files) {
     const e = document.createElement("li");
-    e.textContent = file;
-    e.addEventListener('dblclick', function(e) {
-      // @ts-ignore
-      // factor out upper render logic into function that inits with hardcoded string but then fires with e
-      // but need to get full string to combine with `path` above 
-      render_workflow(pathlib.resolve(path, this.textContent));
-    });
+    e.textContent = file.name;
+
+    var callback;
+    if (file.isDirectory()) {
+      callback = function (e: MouseEvent) {
+        // TODO
+        console.log("i am directory");
+      }
+
+    } else if (file.isFile()) {
+      callback = function (e: MouseEvent) {
+        render_workflow(pathlib.resolve(path, this.textContent!));
+      }
+    } else {
+      throw new Error("found dirent with strange type");
+    }
+
+    e.addEventListener('dblclick', callback);
     file_list.appendChild(e);
   }
 });
